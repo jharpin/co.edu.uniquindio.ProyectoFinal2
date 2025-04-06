705fdd3abb005d35832bca701ee228382aab6055
Manuela-Puerta-A <Manuela.puertaa@uqvirtual.edu.co>
2025-04-06 01:39:44 -0500

    commits

--- src/main/java/co/edu/uniquindio/proyecto/viewController/UsuarioviewController.java
+++ src/main/java/co/edu/uniquindio/proyecto/viewController/UsuarioviewController.java
@@ -1,4 +1,63 @@
 package co.edu.uniquindio.proyecto.viewController;
-
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 public class UsuarioviewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnRegistrarUsuario;
+
+    @FXML
+    private Label lblBilleteraVirtual;
+
+    @FXML
+    private Label lblContrasenia;
+
+    @FXML
+    private Label lblEmailUsuario;
+
+    @FXML
+    private Label lblIdUsuario;
+
+    @FXML
+    private Label lblRegistroTl;
+
+    @FXML
+    private Label lblTelefonoUsuario;
+
+    @FXML
+    private Label lblnombreUsuario;
+
+    @FXML
+    private Hyperlink linkInicioSesion;
+
+    @FXML
+    private TextField txtEmailUsuario;
+
+    @FXML
+    private TextField txtIdeusuario;
+
+    @FXML
+    private TextField txtNombreUsuario;
+
+    @FXML
+    private TextField txtTelefonoUsuario;
+
+    @FXML
+    private TextField txtcontrasenia;
+
+    @FXML
+    void initialize() {
+    }
 }
+

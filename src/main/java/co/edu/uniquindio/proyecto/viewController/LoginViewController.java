b52d5fd9a05cfcd5d54dbece59087edcd5f42d34
Manuela-Puerta-A <Manuela.puertaa@uqvirtual.edu.co>
2025-04-06 01:28:31 -0500

    commits

--- src/main/java/co/edu/uniquindio/proyecto/viewController/LoginViewController.java
+++ src/main/java/co/edu/uniquindio/proyecto/viewController/LoginViewController.java
@@ -1,4 +1,47 @@
 package co.edu.uniquindio.proyecto.viewController;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 public class LoginViewController {
+
+        @FXML
+        private ResourceBundle resources;
+
+        @FXML
+        private URL location;
+
+        @FXML
+        private ImageView btnLogin;
+
+        @FXML
+        private AnchorPane imgLogin;
+
+        @FXML
+        private Label lblBilleteraVirtual;
+
+        @FXML
+        private Label lblContraseniaLogin;
+
+        @FXML
+        private Label lblIdentificacionLogin;
+
+        @FXML
+        private Label lblLogintl;
+
+        @FXML
+        private TextField txtContraseniaLogin;
+
+        @FXML
+        private TextField txtIdentificacionLogin;
+
+        @FXML
+        void initialize() {
+
+        }
 }

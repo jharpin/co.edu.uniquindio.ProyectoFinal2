884940e7483d7855b3943bcf8aa48a5851897ee0
Manuela-Puerta-A <Manuela.puertaa@uqvirtual.edu.co>
2025-04-06 01:30:10 -0500

    commits

--- src/main/java/co/edu/uniquindio/proyecto/viewController/LoginViewController.java
+++ src/main/java/co/edu/uniquindio/proyecto/viewController/LoginViewController.java
@@ -1,6 +1,7 @@
 package co.edu.uniquindio.proyecto.viewController;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
@@ -10,38 +11,39 @@ import java.util.ResourceBundle;
 
 public class LoginViewController {
 
-        @FXML
-        private ResourceBundle resources;
+    @FXML
+    private ResourceBundle resources;
 
-        @FXML
-        private URL location;
+    @FXML
+    private URL location;
 
-        @FXML
-        private ImageView btnLogin;
+    @FXML
+    private ImageView btnLogin;
 
-        @FXML
-        private AnchorPane imgLogin;
+    @FXML
+    private AnchorPane imgLogin;
 
-        @FXML
-        private Label lblBilleteraVirtual;
+    @FXML
+    private Label lblBilleteraVirtual;
 
-        @FXML
-        private Label lblContraseniaLogin;
+    @FXML
+    private Label lblContraseniaLogin;
 
-        @FXML
-        private Label lblIdentificacionLogin;
+    @FXML
+    private Label lblIdentificacionLogin;
 
-        @FXML
-        private Label lblLogintl;
+    @FXML
+    private Label lblLogintl;
 
-        @FXML
-        private TextField txtContraseniaLogin;
+    @FXML
+    private Hyperlink linkOlvidoContrasenia;
 
-        @FXML
-        private TextField txtIdentificacionLogin;
+    @FXML
+    private Hyperlink linkRegistrarCuenta;
 
-        @FXML
-        void initialize() {
+    @FXML
+    private TextField txtContraseniaLogin;
 
-        }
+    @FXML
+    private TextField txtIdentificacionLogin;
 }

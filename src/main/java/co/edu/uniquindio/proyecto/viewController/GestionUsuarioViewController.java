9379c36869373be32493912469c58c484be7cb96
Manuela-Puerta-A <Manuela.puertaa@uqvirtual.edu.co>
2025-04-06 13:39:25 -0500

    commits

---
+++ src/main/java/co/edu/uniquindio/proyecto/viewController/GestionUsuarioViewController.java
@@ -0,0 +1,59 @@
+package co.edu.uniquindio.proyecto.viewController;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+
+public class GestionUsuarioViewController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnActualizarUsuario;
+
+    @FXML
+    private Button btnCrearUsuario;
+
+    @FXML
+    private Button btnEliminarUsuario;
+
+    @FXML
+    private Label lblBilleteraVirtual;
+
+    @FXML
+    private Label lblLogintl;
+
+    @FXML
+    private TableView<?> tableUsuariosmostrar;
+
+    @FXML
+    void ActualizarUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void Eliminarusuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void irCrearUsuario(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mostrarusuarios(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {}
+}

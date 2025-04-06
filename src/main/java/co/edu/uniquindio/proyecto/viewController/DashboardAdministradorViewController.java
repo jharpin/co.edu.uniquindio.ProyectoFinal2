0f3ad6833bb9e151226d7c9787fac51cb6dad137
Manuela-Puerta-A <Manuela.puertaa@uqvirtual.edu.co>
2025-04-06 02:01:23 -0500

    comirs

--- src/main/java/co/edu/uniquindio/proyecto/viewController/DashboardAdministradorViewController.java
+++ src/main/java/co/edu/uniquindio/proyecto/viewController/DashboardAdministradorViewController.java
@@ -1,4 +1,59 @@
 package co.edu.uniquindio.proyecto.viewController;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
 
 public class DashboardAdministradorViewController {
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button btnActualizarDatosAdmi;
+
+    @FXML
+    private Label lActualizarAdmi;
+
+    @FXML
+    private Label lblActualizar;
+
+    @FXML
+    private Label lblActualizarDireccionAd;
+
+    @FXML
+    private Label lblBilleteraVirtual;
+
+    @FXML
+    private Label lblEmailActualizar;
+
+    @FXML
+    private Label lblnombreActualizar;
+
+    @FXML
+    private Tab seccionActualizar;
+
+    @FXML
+    private TableColumn<?, ?> tablaDatosAdmi;
+
+    @FXML
+    private TextField txtDireccionadAct;
+
+    @FXML
+    private TextField txtEmailAdmAct;
+
+    @FXML
+    private TextField txtNombreAdmActu;
+
+    @FXML
+    private TextField txtTelefAdmAct;
+
+    @FXML
+    void initialize() {}
 }

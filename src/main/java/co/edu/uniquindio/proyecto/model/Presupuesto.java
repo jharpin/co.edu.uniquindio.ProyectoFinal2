c14a548e623a77a85aa27dd304cf56c13a8fc1fc
Manuela-Puerta-A <Manuela.puertaa@uqvirtual.edu.co>
2025-04-06 01:11:42 -0500

    commits

--- src/main/java/co/edu/uniquindio/proyecto/model/Presupuesto.java
+++ src/main/java/co/edu/uniquindio/proyecto/model/Presupuesto.java
@@ -1,4 +1,22 @@
 package co.edu.uniquindio.proyecto.model;
 
 public class Presupuesto {
+    private String nombrePresupuesto;
+    private String idPresupuesto;
+    private int montoAsignado;
+    private int montoGastado;
+    private Categoria categoria;
+
+    public Presupuesto() {}
+
+    public String getNombrePresupuesto() {return nombrePresupuesto;}
+    public void setNombrePresupuesto(String nombrePresupuesto) {this.nombrePresupuesto = nombrePresupuesto;}
+    public String getIdPresupuesto() {return idPresupuesto;}
+    public void setIdPresupuesto(String idPresupuesto) {this.idPresupuesto = idPresupuesto;}
+    public int getMontoAsignado() {return montoAsignado;}
+    public void setMontoAsignado(int montoAsignado) {this.montoAsignado = montoAsignado;}
+    public int getMontoGastado() {return montoGastado;}
+    public void setMontoGastado(int montoGastado) {this.montoGastado = montoGastado;}
+    public Categoria getCategoria() {return categoria;}public void setCategoria(Categoria categoria) {this.categoria = categoria;}
+
 }
--- src/main/java/co/edu/uniquindio/proyecto/model/Transaccion.java
+++ src/main/java/co/edu/uniquindio/proyecto/model/Transaccion.java
@@ -1,4 +1,33 @@
 package co.edu.uniquindio.proyecto.model;
 
+import java.util.Date;
+
 public class Transaccion {
+    private Cuenta cuenta;
+    private Banco banco;
+    private String idTransaccion;
+    private Date fechaTransaccion;
+    private String estadoTransaccion;
+
+    public Cuenta getCuenta() {return cuenta;}
+    public void setCuenta(Cuenta cuenta) {this.cuenta = cuenta;}
+    public String getEstadoTransaccion() {return estadoTransaccion;}
+    public void setEstadoTransaccion(String estadoTransaccion) {this.estadoTransaccion = estadoTransaccion;}
+    public Date getFechaTransaccion() {return fechaTransaccion;}
+    public void setFechaTransaccion(Date fechaTransaccion) {this.fechaTransaccion = fechaTransaccion;}
+    public Banco getBanco() {return banco;}
+    public void setBanco(Banco banco) {this.banco = banco;}
+    public String getIdTransaccion() {return idTransaccion;}
+    public void setIdTransaccion(String idTransaccion) {this.idTransaccion = idTransaccion;}
+
+    @Override
+    public String toString() {
+        return "Transaccion{" +
+                "cuenta=" + cuenta +
+                ", banco=" + banco +
+                ", idTransaccion='" + idTransaccion + '\'' +
+                ", fechaTransaccion=" + fechaTransaccion +
+                ", estadoTransaccion='" + estadoTransaccion + '\'' +
+                '}';
+    }
 }

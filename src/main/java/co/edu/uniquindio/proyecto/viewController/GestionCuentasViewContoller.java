7082f2b4db98c067fd11918332a35f3721532a81
jharpin <154296932+jharpin@users.noreply.github.com>
2025-05-27 12:28:02 -0500

    Cambios

--- src/main/java/co/edu/uniquindio/proyecto/viewController/GestionCuentasViewContoller.java
+++ src/main/java/co/edu/uniquindio/proyecto/viewController/GestionCuentasViewContoller.java
@@ -174,48 +174,21 @@ public class GestionCuentasViewContoller {
     private void cargarDatosTabla() {
         tableCuentas.setItems(listaCuenta);
     }
-<<<<<<< HEAD
-    
-=======
-
 
 
 
     @FXML
     void onEliminar(ActionEvent event) {
+        CuentaDto seleccionada = tableCuentas.getSelectionModel().getSelectedItem();
 
-            CuentaDto seleccionada = tableCuentas.getSelectionModel().getSelectedItem();
-
-            if (seleccionada == null) {
-                mostrarAlerta("Error", "Debes seleccionar una cuenta para eliminar.");
-                return;
-            }
-
-            Alert confirmacion = new Alert(Alert.AlertType.CONFIRMATION);
-            confirmacion.setTitle("Confirmación");
-            confirmacion.setHeaderText("¿Eliminar cuenta?");
-            confirmacion.setContentText("Cuenta: " + seleccionada.nombreCuenta());
-
-            confirmacion.showAndWait().ifPresent(respuesta -> {
-                if (respuesta == ButtonType.OK) {
-                    // Usamos solo el ID
-                    String idAEliminar = seleccionada.idCuenta();
-
-                    boolean eliminada = cuentaController.eliminarCuenta(idAEliminar);
-
-                    if (eliminada) {
-                        listaCuenta.remove(seleccionada); // elimina visual
-                        tableCuentas.refresh();
-                        limpiarCampos();
-                        mostrarAlerta("Éxito", "Cuenta eliminada correctamente.");
-                    } else {
-                        mostrarAlerta("Error", "No se pudo eliminar. Verifica el ID.");
-                    }
-                }
-            });
+        if (seleccionada != null) {
+            cuentaController.eliminarCuenta(seleccionada.idCuenta());
+            listaCuenta.removeIf(c -> c.idCuenta().equals(seleccionada.idCuenta()));
+            tableCuentas.refresh();
         }
+    }
 
->>>>>>> 9ebb10a163773e53259d62d026e0dc4a9b64c836
+
 
 
     private void mostrarAlerta(String titulo, String mensaje) {

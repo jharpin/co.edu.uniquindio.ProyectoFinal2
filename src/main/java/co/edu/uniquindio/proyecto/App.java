e760d95b7315c35146bd978c37b7f78b4e8ba1a5
jharpin <154296932+jharpin@users.noreply.github.com>
2025-05-27 15:48:49 -0500

    cambio

--- src/main/java/co/edu/uniquindio/proyecto/App.java
+++ src/main/java/co/edu/uniquindio/proyecto/App.java
@@ -11,7 +11,7 @@ public class App extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Billetera Virtual!");
+        stage.setTitle("Billetera Virtual");
         stage.setScene(scene);
 
         stage.sizeToScene();
